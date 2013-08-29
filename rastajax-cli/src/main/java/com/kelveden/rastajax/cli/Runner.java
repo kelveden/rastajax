17cc03d672ce58723d2af31add3dd402d44a26ff
Alistair Dutton <alistair.dutton@nokia.com>
2013-08-29 22:24:59 +0100

    Bit of a tidy up of the CLI code.

--- rastajax-cli/src/main/java/com/kelveden/rastajax/cli/Runner.java
+++ rastajax-cli/src/main/java/com/kelveden/rastajax/cli/Runner.java
@@ -31,6 +31,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
@@ -39,55 +40,33 @@ public class Runner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
+    private static final String SUB_TEXT_PADDING = "    ";
+    private static final String SUB_SUB_TEXT_PADDING = "       - ";
+
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_BLACK = "\u001B[30m";
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
     private static final String ANSI_BLUE = "\u001B[34m";
-    private static final String ANSI_PURPLE = "\u001B[35m";
     private static final String ANSI_CYAN = "\u001B[36m";
-    private static final String ANSI_WHITE = "\u001B[37m";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws CliExecutionExecution {
+
+        if (args.length == 0) {
+            System.out.println("Usage: rastajax <war file> [<comma-separated packages]");
+            return;
+        }
 
         final File warFile = args.length > 0 ? new File(args[0]) : findWar();
         final String packages = args.length > 1 ? args[1] : "com,org,net";
 
         LOGGER.info("Loading war from " + warFile.getAbsolutePath());
 
-        final File tempFolder = new File(FileUtils.getTempDirectory(), "rastajax-explode");
-        FileUtils.forceMkdir(tempFolder);
-        FileUtils.cleanDirectory(tempFolder);
+        final File workingDirectory = ensureWorkingDirectory();
+        extractWarFile(warFile, workingDirectory);
 
+        final List<URL> urls = getClasspathURLs(workingDirectory);
 
-        try {
-            ZipFile zipFile = new ZipFile(warFile);
-            zipFile.extractAll(tempFolder.getAbsolutePath());
-
-        } catch (ZipException e) {
-            e.printStackTrace();
-        }
-
-        final File classesFolder = new File(tempFolder, "WEB-INF/classes");
-        final File libFolder = new File(tempFolder, "WEB-INF/lib");
-
-        final List<URL> urls = new ArrayList<URL>();
-        urls.add(classesFolder.toURI().toURL());
-
-        for (File file : FileUtils.listFiles(libFolder, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)) {
-            urls.add(file.toURI().toURL());
-        };
-
-        final ClassLoader classLoader = new URLClassLoader(urls.toArray(new URL[] {}), Runner.class.getClassLoader());
-
-        final ClassLoaderRootResourceScanner scanner = new ClassLoaderRootResourceScanner(
-                classLoader,
-                packages.split(",")).allowInterfaceInheritance();
-
-        final Set<FlatResource> representation = RestDescriber.describeApplication(
-                scanner.scan(),
-                new FlatRepresentationBuilder());
+        final Set<FlatResource> representation = loadRepresentation(urls, packages);
 
         for (FlatResource f : representation) {
             printInfo("");
@@ -96,6 +75,69 @@ public class Runner {
                 writeResourceMethod(m);
             }
         }
+    }
+
+    private static Set<FlatResource> loadRepresentation(List<URL> urls, String packages) {
+
+        final ClassLoader classLoader = new URLClassLoader(urls.toArray(new URL[] {}), Runner.class.getClassLoader());
+
+        final ClassLoaderRootResourceScanner scanner = new ClassLoaderRootResourceScanner(
+                classLoader,
+                packages.split(",")).allowInterfaceInheritance();
+
+        return RestDescriber.describeApplication(
+                scanner.scan(),
+                new FlatRepresentationBuilder());
+    }
+
+    private static List<URL> getClasspathURLs(File workingDirectory) throws CliExecutionExecution {
+
+        final File classesFolder = new File(workingDirectory, "WEB-INF/classes");
+        final File libFolder = new File(workingDirectory, "WEB-INF/lib");
+
+        final List<URL> urls = new ArrayList<URL>();
+        try {
+            urls.add(classesFolder.toURI().toURL());
+
+        } catch (final MalformedURLException e) {
+            throw new CliExecutionExecution("Could not get the URL for '" + classesFolder.getAbsolutePath() + "'.", e);
+        }
+
+        for (File file : FileUtils.listFiles(libFolder, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)) {
+            try {
+                urls.add(file.toURI().toURL());
+
+            } catch (final MalformedURLException e) {
+                throw new CliExecutionExecution("Could not get the URL for '" + file.getAbsolutePath() + "'.", e);
+            }
+        }
+
+        return urls;
+    }
+
+    private static void extractWarFile(File warFile, File workingDirectory) throws CliExecutionExecution {
+        try {
+            ZipFile zipFile = new ZipFile(warFile);
+            zipFile.extractAll(workingDirectory.getAbsolutePath());
+
+        } catch (final ZipException e) {
+            throw new CliExecutionExecution("Could not extract WAR file.", e);
+        }
+    }
+
+    private static File ensureWorkingDirectory() throws CliExecutionExecution {
+
+        final File tempFolder = new File(FileUtils.getTempDirectory(), "rastajax-explode");
+
+        try {
+            FileUtils.forceMkdir(tempFolder);
+            FileUtils.cleanDirectory(tempFolder);
+
+        } catch (final IOException e) {
+            throw new CliExecutionExecution("Failed to ensure presence of working folder.", e);
+        }
+
+        return tempFolder;
     }
 
     private static File findWar() {
@@ -112,17 +154,11 @@ public class Runner {
 
     private static void writeResourceMethod(FlatResourceMethod resourceMethod) {
 
-        printInfo("    " + ANSI_CYAN + resourceMethod.getRequestMethodDesignator() + ANSI_RESET + " [" + resourceMethod.getResourceClass() + "]");
+        printInfo(SUB_TEXT_PADDING + ANSI_CYAN + resourceMethod.getRequestMethodDesignator() + ANSI_RESET + " [" + resourceMethod.getResourceClass() + "]");
 
-        printInfo("       - " + ANSI_RED + resourceMethod.getName() + ANSI_RESET + "(" + parametersToString(resourceMethod) + ")");
+        printInfo(SUB_SUB_TEXT_PADDING + ANSI_RED + resourceMethod.getName() + ANSI_RESET + "(" + parametersToString(resourceMethod) + ")");
         printInfo(mediaTypesToString(ANSI_CYAN + "Produces: " + ANSI_RESET, resourceMethod.getProduces()));
         printInfo(mediaTypesToString(ANSI_BLUE + "Consumes: " + ANSI_RESET, resourceMethod.getConsumes()));
-    }
-
-    private static void printInfo(final String info) {
-        if (info != null) {
-            System.out.println(info);
-        }
     }
 
     private static String parametersToString(final FlatResourceMethod method) {
@@ -143,7 +179,13 @@ public class Runner {
         if (mediaTypes.size() == 0) {
             return null;
         } else {
-            return "       - " + header + StringUtils.join(mediaTypes.toArray(new String[mediaTypes.size()]), ", ");
+            return SUB_SUB_TEXT_PADDING + header + StringUtils.join(mediaTypes.toArray(new String[mediaTypes.size()]), ", ");
+        }
+    }
+
+    private static void printInfo(final String info) {
+        if (info != null) {
+            System.out.println(info);
         }
     }
 }
